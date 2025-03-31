4183218d7e5798c2d0c59fb9e87d07be3009017a
Navya <you@example.com>
2025-03-31 13:58:12 +0530

    jkhsdfsd

--- backend/index.js
+++ backend/index.js
@@ -75,7 +75,7 @@ mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log("App connected to database");
-    app.listen(PORT, () => {
+    app.listen(process.env.PORT, () => {
       console.log(`App is listening to port: ${process.env.PORT}`);
     });
   })
